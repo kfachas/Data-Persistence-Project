8b177eff83d51c31c458f033caf72763b8b26408
Kevin <kfachas@gmail.com>
2025-05-10 17:52:23 +0200

    add persistent data config

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,76 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+    public static MainManager Instance;
+    public string nickname;
+    public int highScore;
+    private string saveFilePath = Application.persistentDataPath + "/savefile.json";
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
-
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        if (Instance != null)
         {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+            Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadSaveData();
     }
 
-    private void Update()
+    [System.Serializable]
+    public class SaveData
     {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
-
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
+        public int highScore;
+        public string nickname;
     }
 
-    void AddPoint(int point)
+    public void SaveHighScore(int currentScore)
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        SaveData data = new SaveData();
+
+        if (File.Exists(saveFilePath))
+        {
+            string savedJson = File.ReadAllText(saveFilePath);
+            SaveData savedData = JsonUtility.FromJson<SaveData>(savedJson);
+
+            if (currentScore > savedData.highScore)
+            {
+                data.highScore = currentScore;
+            }
+            data.nickname = savedData.nickname;
+        }
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(saveFilePath, json);
+    }
+    public void SaveNickname()
+    {
+        SaveData data = new SaveData { nickname = nickname };
+
+
+        if (File.Exists(saveFilePath))
+        {
+            string savedJson = File.ReadAllText(saveFilePath);
+            SaveData savedData = JsonUtility.FromJson<SaveData>(savedJson);
+
+            data.highScore = savedData.highScore;
+        }
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(saveFilePath, json);
     }
 
-    public void GameOver()
+    public void LoadSaveData()
     {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+        if (File.Exists(saveFilePath))
+        {
+            string savedJson = File.ReadAllText(saveFilePath);
+            SaveData savedData = JsonUtility.FromJson<SaveData>(savedJson);
+
+            highScore = savedData.highScore;
+            nickname = savedData.nickname;
+        }
     }
 }
